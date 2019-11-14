20c9ceb988ba2609d1676600a662893aa7ef8271
Shohidul Islam <hridoy.jurain@yahoo.com>
2019-11-14 23:45:59 +0600

    az

--- files/ScriptAll_invalidated.sql
+++ files/ScriptAll_invalidated.sql
@@ -1,4 +1,4 @@
 update opdef set xdate = GETDATE() where xacc = 'License Renew'
 
---update opdef set zactive = '1' where xacc = 'License Renew'
+update opdef set zactive = '0' where xacc = 'License Renew'
 update zbusiness set zactive = '0' where zid = '200020';  
