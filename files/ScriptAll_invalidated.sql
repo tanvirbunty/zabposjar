4b34c375ef7b518cb158686cccf36454ba3aab74
Shohidul Islam <hridoy.jurain@yahoo.com>
2019-11-14 15:56:49 +0600

    Update ScriptAll_invalidated.sql
    1122

--- files/ScriptAll_invalidated.sql
+++ files/ScriptAll_invalidated.sql
@@ -1,2 +1,2 @@
-  update zbusiness set zactive = '0' where zid = '200020'
-  update zbusiness set xzip = '0' where zid = '200020'
+  update zbusiness set zactive = '0' where zid = '200020';
+  update zbusiness set xzip = '0' where zid = '200020';
