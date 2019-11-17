853b04b3231d6d965651eade695403cf171897e8
Shohidul Islam <hridoy.jurain@yahoo.com>
2019-11-17 20:40:37 +0600

    0

--- files/ScriptAll_invalidated.sql
+++ files/ScriptAll_invalidated.sql
@@ -1,5 +1,5 @@
 update opdef set xdate = GETDATE() where xacc = 'License Renew'
 
 -- stop pos manullay from cloud
-update opdef set zactive = '1' where xacc = 'License Renew'
-update zbusiness set zactive = '1' where zid = '200020';  
+--update opdef set zactive = '1' where xacc = 'License Renew'
+--update zbusiness set zactive = '1' where zid = '200020';  
