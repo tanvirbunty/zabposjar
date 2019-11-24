42a5181f25404c8923bf5f8085bd71ec95477136
Shohidul Islam <hridoy.jurain@yahoo.com>
2019-11-24 13:31:33 +0600

    hold bill

--- files/ScriptAll.sql
+++ files/ScriptAll.sql
@@ -3,3 +3,6 @@ update opdef set xdate = GETDATE() where xacc = 'License Renew'
 -- stop pos manullay from cloud
 --update opdef set zactive = '1' where xacc = 'License Renew'
 --update zbusiness set zactive = '1' where zid = '200020';  
+
+--temporary remove minimum after 3 days
+insert into opdef (xacc, zactive, zauserid, zid, ztime, zutime, zuuserid) values('Hold Bill', '1', 'zabadmin', 000000, GETDATE(), GETDATE(), 'zabadmin')
