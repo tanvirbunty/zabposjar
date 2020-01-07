930159df0cdba0dde157e0d752490cb0c7a918c4
tarafat <tanvirarafat02@gmail.com>
2020-01-07 14:29:19 +0600

    Delete Old sales Data

--- files/ScriptAll.sql
+++ files/ScriptAll.sql
@@ -10,8 +10,8 @@ DECLARE
 SELECT @zid=zid from zbusiness
 IF @zid<>'100070'
 BEGIN
-DELETE from opdoheader where xdate<EOMONTH(dateadd(m,-2,GETDATE())) 
-DELETE from opdodetail where xdornum IN(SELECT xdornum from opdoheader where xdate<EOMONTH(dateadd(m,-2,GETDATE())))
 DELETE from opposmultiplecard where zutime<EOMONTH(dateadd(m,-2,GETDATE()))
+DELETE from opdodetail where xdornum IN(SELECT xdornum from opdoheader where xdate<EOMONTH(dateadd(m,-2,GETDATE())))
+DELETE from opdoheader where xdate<EOMONTH(dateadd(m,-2,GETDATE())) 
 END
 
