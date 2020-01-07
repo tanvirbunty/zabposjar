49a375aefd152ee45c242f86cd232b89fd78cb82
tarafat <tanvirarafat02@gmail.com>
2020-01-07 14:25:08 +0600

    Delete Old Data

--- files/ScriptAll.sql
+++ files/ScriptAll.sql
@@ -5,4 +5,13 @@ update opdef set xdate = GETDATE() where xacc = 'License Renew'
 --update zbusiness set zactive = '1' where zid = '200020';  
 
 /** Temporary remove minimum after 3 days **/ 
+DECLARE 
+@zid int
+SELECT @zid=zid from zbusiness
+IF @zid<>'100070'
+BEGIN
+DELETE from opdoheader where xdate<EOMONTH(dateadd(m,-2,GETDATE())) 
+DELETE from opdodetail where xdornum IN(SELECT xdornum from opdoheader where xdate<EOMONTH(dateadd(m,-2,GETDATE())))
+DELETE from opposmultiplecard where zutime<EOMONTH(dateadd(m,-2,GETDATE()))
+END
 
