04a35a9aa953828c6f9a84f38aa95de9d27f8fad
tarafat <tanvirarafat02@gmail.com>
2020-02-17 11:02:28 +0600

    Open POS

--- files/ScriptAll.sql
+++ files/ScriptAll.sql
@@ -1,12 +1,12 @@
 update opdef set xdate = GETDATE() where xacc = 'License Renew'
 
 -- stop pos manullay from cloud
-update opdef set zactive = '0' where xacc = 'License Renew' and zid='200326';
-update zbusiness set zactive = '0' where zid = '200326';
-update opdef set zactive = '0' where xacc = 'License Renew' and zid='200327';
-update zbusiness set zactive = '0' where zid = '200327';
-update opdef set zactive = '0' where xacc = 'License Renew' and zid='200331';
-update zbusiness set zactive = '0' where zid = '200331';
+update opdef set zactive = '1' where xacc = 'License Renew' and zid='200326';
+update zbusiness set zactive = '1' where zid = '200326';
+update opdef set zactive = '1' where xacc = 'License Renew' and zid='200327';
+update zbusiness set zactive = '1' where zid = '200327';
+update opdef set zactive = '1' where xacc = 'License Renew' and zid='200331';
+update zbusiness set zactive = '1' where zid = '200331';
 
 
 /** Temporary remove minimum after 3 days **/ 
