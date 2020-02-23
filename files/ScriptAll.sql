eefed0301a27543bb7416595bf8f8b801577355c
tarafat <tanvirarafat02@gmail.com>
2020-02-23 19:01:57 +0600

    Update opdodetail

--- files/ScriptAll.sql
+++ files/ScriptAll.sql
@@ -1,15 +1,15 @@
 update opdef set xdate = GETDATE() where xacc = 'License Renew'
 
 -- stop pos manullay from cloud
+/**
 update opdef set zactive = '1' where xacc = 'License Renew' and zid='200326';
 update zbusiness set zactive = '1' where zid = '200326';
-update opdef set zactive = '1' where xacc = 'License Renew' and zid='200327';
-update zbusiness set zactive = '1' where zid = '200327';
-update opdef set zactive = '1' where xacc = 'License Renew' and zid='200331';
-update zbusiness set zactive = '1' where zid = '200331';
-
+**/
 
 /** Temporary remove minimum after 3 days **/ 
+ALTER TABLE opdodetail
+ALTER COLUMN xqtyord  numeric(20,3);
+
 DECLARE 
 @zid int
 SELECT @zid=zid from zbusiness
