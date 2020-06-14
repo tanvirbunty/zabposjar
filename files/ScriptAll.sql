ffa4b5b052f15664e9e018af25088bf310d59d98
tarafat <tanvirarafat02@gmail.com>
2020-06-14 14:22:08 +0600

    food service

--- files/ScriptAll.sql
+++ files/ScriptAll.sql
@@ -4,19 +4,21 @@ update opdef set xdate = GETDATE() where xacc = 'License Renew'
 /**
 update opdef set zactive = '1' where xacc = 'License Renew' and zid='200326';
 update zbusiness set zactive = '1' where zid = '200326';
+
 **/
 
 /** Temporary remove minimum after 3 days **/ 
-ALTER TABLE opdodetail
-ALTER COLUMN xqtyord  numeric(20,3);
+--ALTER TABLE opdodetail
+--ALTER COLUMN xqtyord  numeric(20,3);
 
---DECLARE 
---@zid int
---SELECT @zid=zid from zbusiness
---IF @zid<>'100070'
---BEGIN
---DELETE from opposmultiplecard where zutime<EOMONTH(dateadd(m,-2,GETDATE()))
---DELETE from opdodetail where xdornum IN(SELECT xdornum from opdoheader where xdate<EOMONTH(dateadd(m,-2,GETDATE())))
---DELETE from opdoheader where xdate<EOMONTH(dateadd(m,-2,GETDATE())) 
---END
+DECLARE 
+@zid int,
+@xshopno varchar(20) 
+SELECT @zid=zid from zbusiness
+select @xshopno=xshopno from xtrn where xtypetrn='Item Code' and zid=@zid
+IF @zid<>'100070'
+BEGIN
+INSERT INTO xcodes(xcode,xtype,xshopno,zactive,zauserid,zid,ztime) values('Evaly Express','Delivery Service',@xshopno,'1','zabadmin',@zid,GETDATE());
+INSERT INTO xcodes(xcode,xtype,xshopno,zactive,zauserid,zid,ztime) values('Pathao Food','Delivery Service',@xshopno,'1','zabadmin',@zid,GETDATE());
+END
 
