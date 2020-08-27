33d0a68275cd6f1683c67edbdb479f28177479b9
ZABERP-FORHAD <60842656+ZABERP-FORHAD@users.noreply.github.com>
2020-08-27 13:45:26 +0600

    Reguler Maintenance

--- files/ScriptAll.sql
+++ files/ScriptAll.sql
@@ -2,8 +2,8 @@ update opdef set xdate = GETDATE() where xacc = 'License Renew'
 
 -- stop pos manullay from cloud
 /**
-update opdef set zactive = '1' where xacc = 'License Renew' and zid='200326';
-update zbusiness set zactive = '1' where zid = '200326';
+-- update opdef set zactive = '1' where xacc = 'License Renew' and zid='200326';
+-- update zbusiness set zactive = '1' where zid = '200326';
 
 **/
 
